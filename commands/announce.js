c8b7eacf7ef26aac7644bebc3da2090aeda0d0ea
Nicholas Caro <caronicholas89813@gmail.com>
2017-04-28 18:27:46 -0400

    Added announce command.

---
+++ commands/announce.js
@@ -0,0 +1,21 @@
+exports.run = (client, message, args) => {
+  let announceMsg = args.slice(1).join(" ");
+  let announceChannel = message.mentions.channels.first();
+  if (announceMsg.length < 1) return message.reply("What am I announcing?");
+  if (message.mentions.channels.size < 1) return message.reply("I can\'t announce without having a channel to say the announcement.");
+
+  announceChannel.sendMessage("@here " + announceMsg);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['say'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'announce',
+  description: 'Use to send an announcement to a given channel',
+  usage: 'announce [channel mention] [message]'
+};
