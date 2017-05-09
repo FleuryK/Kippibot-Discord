7b2ccb3fd45c118b346522666426afef91249f9d
Nicholas Caro <caronicholas89813@gmail.com>
2017-05-08 23:31:28 -0400

    Revised 8-ball

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,14 +1,32 @@
-const request = require("request");
+const responses = [
+  "As I see it, yes.",
+  "Ask again later.",
+  "Better not tell you now.",
+  "Cannot predict now.",
+  "Concentrate and ask again.",
+  "Don't count on it.",
+  "It is certain.",
+  "It is decidely so.",
+  "Most likely.",
+  "My reply is no.",
+  "My sources say no.",
+  "Outlook good.",
+  "Outlook not so good.",
+  "Reply hazy, try again.",
+  "Signs point to yes.",
+  "Very doubtful.",
+  "Without a doubt.",
+  "Yes",
+  "Yes, definitely.",
+  "You may rely on it."
+];
 exports.run = (client, message, params) => {
   if (!params[0]) {
     return message.channel.send("The Magic Kippibot 8-ball cannot predict a blank fortune.");
   }
-  request("https://api.rtainc.co/twitch/8ball?format=The+Magic+Kippibot+8-Ball+predicts...+%5B0%5D", function (err, res, body) {
-    if (err || res.statusCode !== 200) {
-      message.channel.send("The Magic Kippibot 8-ball is currently broken, try again later.");
-    }
-    else { message.channel.send(body, {code: 'js'}); }
-  });
+  var resNum = Math.floor(Math.random() * responses.length);
+  var msg = "The Magic Kippibot 8-ball predicts... " + responses[resNum];
+  return message.channel.send(msg, {code: 'js'}); 
 };
 exports.conf = {
   enabled: true,
